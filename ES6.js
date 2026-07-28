ab02eaabcd6cebff5940b44afc235f4c44671152
Aniketpatil7278 <142803348+Aniketpatil7278@users.noreply.github.com>
2026-07-28 15:41:32 +0530

    OBj DES

--- ES6.js
+++ ES6.js
@@ -1,1 +1,27 @@
-console.log("nadbnab");
+console.log("-----------------------Template Literals--------------");
+
+let a=10;
+let b=20;
+console.log(`add result of ${a} and ${b} is = ${a+b}`);
+
+
+console.log("--------------Calling Functions--------------");
+const greet=(name)=>{
+    return `my name is ${name}`;
+}
+console.log(greet("Aniket"));
+
+
+
+console.log("------------------Ternary Operator-------------------");
+let marks=20;
+console.log(`status: ${marks >= 35 ? "pass" : "Failes"}`);
+
+let text=`bv
+nsklf
+nskf
+n;snf
+mcsn;
+n;sc
+m;`
+console.log(text);
